4bb1a7f4e1912ca365eea4b9145f25cc7fb4baf3
danielkim321 <dkthor2008@gmail.com>
2025-11-06 09:35:38 -0500

    789

--- fileio.py
+++ fileio.py
@@ -18,7 +18,7 @@ for lines in buffer:
 print(names)
 print(grades)
 
-file = open ("names.txt,w")
+file = open ("names.txt","w")
 line = "hello world"
 file.write(line)
 file.close()
@@ -26,4 +26,29 @@ for i in range(len(names)):
     line = f"{names[i]},{grades[i]}/n"
     buffer.append(line)
     file.write(line)
+
+buffer[-1] = buffer[-1].strip()
+
+file.writelines(buffer)
 file.close()
+
+
+
+try:
+    name = input("enter your nane: ").strip()
+    grade = input("enter your grade:")
+    grade = int(grade)
+except Exception as e:
+    print(e)
+else:
+    file = open("names.txt","a")
+    line = f"/n{names},{grade}"
+    file.write(line)
+    file.close
+    
+    
+    
+    
+    
+    
+``
