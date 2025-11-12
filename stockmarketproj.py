aaa2aa5379eca94d5c6843f0fe6a61ba6bef3ab9
danielkim321 <dkthor2008@gmail.com>
2025-11-12 09:28:32 -0500

    4

--- stockmarketproj.py
+++ stockmarketproj.py
@@ -1,7 +1,20 @@
-file = open "buffer.txt","r"
+file = open ("buffer.txt","r")
 
-buffer.file = file.readlines()
+buffer = file.readlines()
 file.close()
 print(buffer)
 print(buffer[0])
-msft = buffer[0]
+msft = buffer[0].split(",")
+amzn = buffer[1].split(",")
+nvda = buffer[2].split(",")
+msft.pop(0)
+amzn.pop(0)
+nvda.pop(0)
+print(msft)
+print(amzn)
+print(nvda)
+
+
+
+
+
